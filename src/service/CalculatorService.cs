896649aeb5fa64e6d97f49bf52c83c93bcb50a84
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2022-06-24 17:28:05 +0200

    refactored tests

--- src/service/CalculatorService.cs
+++ src/service/CalculatorService.cs
@@ -8,63 +8,77 @@ namespace GoldenCalculator
     {
         public async Task<string> CalculateCircle(string width) => await Task.Run(() =>
         {
-            if(!decimal.TryParse(width, out decimal w))
+            if (!decimal.TryParse(width, out decimal w))
                 throw new FormatException($"Cannot convert width({width}) to decimal.");
 
-            return new CircleModel(w).ToString();
+            decimal _pi = 3.14M;
+            decimal _diameter = w / 1.618M;
+            decimal _radius = _diameter / 2;
+            decimal _circumference = 2 * _pi * _radius;
+
+            return JsonConvert.SerializeObject(new { Circumference = _circumference, Diameter = _diameter, Radius = _radius });
         });
 
-        public async Task<string> CalculateGrid(string width)  => await Task.Run(() =>
+        public async Task<string> CalculateGrid(string width) => await Task.Run(() =>
         {
-            if(!decimal.TryParse(width, out decimal w))
+            if (!decimal.TryParse(width, out decimal w))
                 throw new FormatException($"Cannot convert width({width}) to decimal.");
 
-            return new GridModel(w).ToString();
+            decimal _width = w;
+            decimal _v1 = _width / 3;
+            decimal _v2 = _v1 * 2;
+            decimal _h1 = _v1 / 1.618M;
+            decimal _h2 = _h1 + ( _h1 / 1.618M);
+            decimal _height = (_h1 * 2) + _h1;
+
+            return JsonConvert.SerializeObject(new { Height = _height, Width = _width, V1 = _v1, V2 = _v2, H1 = _h1, H2 = _h2 });
         });
 
-        public async Task<string> CalculateInverseTriangle(string width) => await Task.Run(() =>
+        public Task<string> CalculateInverseTriangle(string width)
         {
-            if(!decimal.TryParse(width, out decimal w))
-                throw new FormatException($"Cannot convert width({width}) to decimal.");
-
-            return new InverseTriangleModel(w).ToString();
-        });
+            throw new NotImplementedException();
+        }
 
         public async Task<string> CalculateRectangle(string width) => await Task.Run(() =>
         {
-            if(!decimal.TryParse(width, out decimal w))
+            if (!decimal.TryParse(width, out decimal w))
                 throw new FormatException($"Cannot convert width({width}) to decimal.");
 
-            return new RectangleModel(w).ToString();
+            decimal _width = w;
+            decimal _height = w / 1.618M;
+
+            return JsonConvert.SerializeObject(new { Height = _height, Width = _width });
         });
 
         public async Task<string> CalculateSquare(string width) => await Task.Run(() =>
         {
-            if(!decimal.TryParse(width, out decimal w))
+            if (!decimal.TryParse(width, out decimal w))
                 throw new FormatException($"Cannot convert width({width}) to decimal.");
 
             decimal _width = w / 1.618M;
             decimal _height = _width;
 
-            var square = new SquareModel{ Width = _width , Height = _height};
-
-            return JsonConvert.SerializeObject(square);
+            return JsonConvert.SerializeObject(new { Height = _height, Width = _width });
         });
 
         public async Task<string> CalculateThird(string width) => await Task.Run(() =>
         {
-            if(!decimal.TryParse(width, out decimal w))
+            if (!decimal.TryParse(width, out decimal w))
                 throw new FormatException($"Cannot convert width({width}) to decimal.");
 
-            return new ThirdModel(w).ToString();
+            decimal _width = w;
+            decimal _v1 = _width/3;
+            decimal _v2 = _v1 * 2;
+            decimal _h1 = _v1 / 1.618M;
+            decimal _h2 = _h1 * 2;
+            decimal _height = _h1 * 3;
+
+            return JsonConvert.SerializeObject(new { Height = _height, Width = _width, V1 = _v1, V2 = _v2, H1 = _h1, H2 = _h2 });
         });
 
-        public async Task<string> CalculateTriangle(string width) => await Task.Run(() =>
+        public Task<string> CalculateTriangle(string width)
         {
-            if(!decimal.TryParse(width, out decimal w))
-                throw new FormatException($"Cannot convert width({width}) to decimal.");
-
-            return new TriangleModel(w).ToString();
-        });
+            throw new NotImplementedException();
+        }
     }
 }
--- test/CalculatorUnitTest.cs
+++ test/CalculatorUnitTest.cs
@@ -5,29 +5,175 @@ using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Extensions.Logging.Abstractions;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Moq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+using Microsoft.Azure.WebJobs;
+using System.Collections.Generic;
+using Xunit.Abstractions;
 
 namespace GoldenCalculator
 {
     public class CalculatorUnitTest
     {
-         [Fact]
-        public void GetSquare_()
+        [Fact]
+        public void GetSquare_Given_CalculatorServiceMock_Should_Return_Expected_Square_Width_And_Height()
         {
-            //Arrange
-            CalculatorService cs = new CalculatorService();
-
-            //Act
-            SquareModel square = Task.Run(async () =>
+            // Arrange
+            var expectedSquare = JsonConvert.SerializeObject(new
             {
-                return JsonConvert.DeserializeObject<SquareModel>(await cs.CalculateSquare("25"));
-            }).Result;
+                Height = 61.804697156983930778739184178M,
+                Width = 61.804697156983930778739184178M
+            });
+
+            var CalculatorServiceMoq = new Mock<ICalculatorService>();
+            CalculatorServiceMoq.Setup<Task<string>>(cs => cs.CalculateSquare(It.IsAny<string>())).Returns(Task.Run(() => { return expectedSquare; }));
+
+            var HttpReqMoq = new Mock<HttpRequest>();
+            var logger = new Mock<Microsoft.Extensions.Logging.ILogger>();
+
+            // Act
+            var response = new Calculator(CalculatorServiceMoq.Object).GetSquare(HttpReqMoq.Object, "100", logger.Object);
             
-            //Assert
-            Assert.Equal(square.Height, square.Width);
-            Assert.Equal(15.451174289245982694684796044M, square.Width);
-            Assert.Equal(15.451174289245982694684796044M, square.Height);
+            // Assert
+            Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+            Assert.Equal(expectedSquare, ((OkObjectResult)response.Result).Value.ToString());
         }
+
+        //  [Fact]
+        // public void GetSquare()
+        // {
+        //     var host = new HostBuilder()
+        //     .UseEnvironment("Development")
+        //         .ConfigureWebJobs(x => {
+        //             x.AddAzureStorage();
+        //             x.AddHttp();
+        //             x.UseExternalStartup();
+                    
+        //         })
+        //         .UseConsoleLifetime()
+        //         .ConfigureServices(s =>
+        //         {
+        //             s.AddSingleton<ICalculatorService>((cs) => { return new CalculatorService(); });
+        //         })
+        //         .Build();
+
+        //     using (host) {
+        //         host.Run();
+                
+        //         HttpClient client = new HttpClient();
+        //         var response = client.GetAsync("http://localhost:7071/api/v1/GetCircle/100");
+        //         response.Wait();
+        //     }
+
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetRectangle()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetRectangle(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetCircle()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetCircle(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetGrid()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetGrid(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetTriangle()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetTriangle(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetInverseTriangle()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetInverseTriangle(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
+        // [Fact]
+        // public void Test_Calculate_GetThird()
+        // {
+        //     var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+        //     var response = new Calculator(new CalculatorService()).GetThird(new DefaultHttpRequest(new DefaultHttpContext()), "1080", logger);
+        //     response.Wait();
+
+        //     Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+
+        //     var actual = (OkObjectResult)response.Result;
+
+        //     //var expected = "{\"width\":667.49072929542645241038318912,\"height\":667.49072929542645241038318912}";
+
+        //     Assert.Equal(actual.Value.ToString(), actual.Value.ToString());
+        // }
+
     }
 }
